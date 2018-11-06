738b9f78e3ec66e70a1e73dc862dacaf7bafd63a
Alpha Mackie <alphamackie@Alphas-MacBook-Pro.local>
2018-11-06 16:27:35 +0000

    Added an exception when max value is exceeded

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,9 +1,14 @@
 class Oystercard
 attr_reader :balance
+MAX_VALUE = 90
 
 def initialize
   @balance = 0
+
+end
+def topup(money)
+  raise'maximum value exceeded' if @balance + money > MAX_VALUE
+  @balance += money
 end
 
 end
- 
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,10 +1,18 @@
 require 'Oystercard'
 
 describe 'Oystercard' do
+  let(:oystercard) { oystercard = Oystercard.new }
   # attr_accessor :balance
 
   it "expects a newly initialized Oystercard to mave a balance of zero" do
-  oystercard = Oystercard.new
   expect(oystercard.balance).to eq (0)
 end
+  it "adds funds to oystercard" do
+    expect{ oystercard.topup 5 }.to change { oystercard.balance }.by 5
+  end
+  it "it throws an exception if balance goes over 90" do
+    max_balance = Oystercard::MAX_VALUE
+    oystercard.topup(max_balance)
+    expect{oystercard.topup 1 }.to raise_error 'maximum value exceeded'
+  end
 end
